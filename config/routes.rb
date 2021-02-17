7153a95a146fb401a80d3f161ca8e67c727805da
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-17 16:09:37 +0900

    rooms controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/new'
+  get 'rooms/create'
+  get 'rooms/show'
+  get 'rooms/edit'
+  get 'rooms/update'
+  get 'rooms/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
