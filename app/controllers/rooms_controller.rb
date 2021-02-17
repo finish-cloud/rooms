f72afc82164323a1ef997533ef2ae4ab3db83e35
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-17 16:39:09 +0900

    crud の情報を更新

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,15 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    room = Room.create!(name: room_params[:name], introduction: room_params[:introduction], charge: room_params[:charge], address: room_params[:address], image: room_params[:image], user_id: current_user.id)
+    redirect_to room
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'rooms/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "rooms#index"
+  resources :rooms
+  resource :users, only: :show
 end
